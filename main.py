50dbf9836c525be3dfba47359528de2a2d081a41
lipimahobiya <psmahobiya@gmail.com>
2025-04-30 20:36:10 +0530

    python operater

---
+++ main.py
@@ -0,0 +1,8 @@
+a = 5
+b = 6
+c = 10
+d = 12
+sum = a + b + c + d
+print(sum)
+avg = sum/4
+print(avg)
---
+++ main1.py
@@ -0,0 +1,7 @@
+maths = int(input("enter your marks "))
+english = int(input("enter your marks "))
+science = int(input("enter your marks "))
+sum = maths + english + science
+print(sum)
+p = (sum/300)*100
+print(p)
